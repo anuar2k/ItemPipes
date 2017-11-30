6c05ab8bc4788f372c2b676d6f10b53128c86120
Michael Pollind <polli104@mail.chapman.edu>
2017-11-30 14:49:54 -0800

    added elements from signalling

--- src/main/java/org/terasology/terapipes/blocks/PipeBlockFamily.java
+++ src/main/java/org/terasology/terapipes/blocks/PipeBlockFamily.java
@@ -15,5 +15,61 @@
  */
 package org.terasology.terapipes.blocks;
 
-public class PipeBlockFamily {
+import gnu.trove.map.TByteObjectMap;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.math.Side;
+import org.terasology.math.SideBitFlag;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.terapipes.components.PipeComponent;
+import org.terasology.world.BlockEntityRegistry;
+import org.terasology.world.WorldProvider;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockUri;
+import org.terasology.world.block.family.UpdatesWithNeighboursFamily;
+
+import java.util.List;
+
+public class PipeBlockFamily extends UpdatesWithNeighboursFamily {
+
+    private byte connectionSides;
+    private TByteObjectMap<Block> blocks;
+
+    public PipeBlockFamily(BlockUri blockUri, List<String> categories, Block archetypeBlock, TByteObjectMap<Block> blocks, byte connectionSides) {
+        super(null, blockUri, categories, archetypeBlock, blocks, connectionSides);
+        this.connectionSides = connectionSides;
+        this.blocks = blocks;
+
+    }
+
+    @Override
+    public Block getBlockForPlacement(WorldProvider worldProvider, BlockEntityRegistry blockEntityRegistry, Vector3i location, Side attachmentSide, Side direction) {
+        byte connections = 0;
+        for (Side connectSide : SideBitFlag.getSides(connectionSides)) {
+            if (connectionCondition(location, connectSide,worldProvider,blockEntityRegistry)) {
+                connections += SideBitFlag.getSide(connectSide);
+            }
+        }
+        return blocks.get(connections);
+    }
+
+    @Override
+    public Block getBlockForNeighborUpdate(WorldProvider worldProvider, BlockEntityRegistry blockEntityRegistry, Vector3i location, Block oldBlock) {
+        byte connections = 0;
+        for (Side connectSide : SideBitFlag.getSides(connectionSides)) {
+            if (connectionCondition(location, connectSide,worldProvider,blockEntityRegistry)) {
+                connections += SideBitFlag.getSide(connectSide);
+            }
+        }
+        return blocks.get(connections);
+    }
+
+
+    public boolean connectionCondition(Vector3i blockLocation, Side connectSide,WorldProvider worldProvider,BlockEntityRegistry blockEntityRegistry) {
+        Vector3i neighborLocation = new Vector3i(blockLocation);
+        neighborLocation.add(connectSide.getVector3i());
+
+
+        EntityRef neighborEntity = blockEntityRegistry.getBlockEntityAt(neighborLocation);
+        return neighborEntity != null && neighborEntity.hasComponent(PipeComponent.class);
+    }
 }
